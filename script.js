73b8bbfcd0e8487b3c1a6137d4a1cf2052f485a0
EveOgutu <120215430+EveOgutu@users.noreply.github.com>
2023-01-31 10:50:45 +0300

    Added script

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const modal = document.getElementById('modal');
 //toggle nav
 toggle.addEventListener('click', () =>
 document.body.classList.toggle('show-nav'));
+
+//Show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+//Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+//hide modal on outside click
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
